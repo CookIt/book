52c5815df6b78cd348984073c7ac077119f4e6d0
Steven Tang <steven.tang@colorado.edu>
2016-02-19 16:19:07 -0700

    merged maps to one

--- src/apps/uber/client/components/app.jsx
+++ src/apps/uber/client/components/app.jsx
@@ -23,22 +23,13 @@ class App extends React.Component {
         <div className="card green darken-2">
         <MyComponents.MapView
             premProviders={this.props.data.premProviders}
+			stdProviders={this.props.data.stdProviders}
             center={this.props.data.center}
             user={this.props.data.user}
             setUserLocationAction={this.props.actions.setUserLocation}/>
           </div>
-
-  <div className="card green darken-2">
-
-      <MyComponents.StdMapView
-            stdProviders={this.props.data.stdProviders}
-            center={this.props.data.center}
-            user={this.props.data.user}
-            setUserLocationAction={this.props.actions.setUserLocation}/>
-
-          </div>
+		  </div>
     </div>
-  </div>
 
   }
 }
--- src/apps/uber/client/components/map.jsx
+++ src/apps/uber/client/components/map.jsx
@@ -1,24 +1,35 @@
 
 const {Map, Marker, CircleMarker, Popup, TileLayer, MapLayer}  = window.ReactLeaflet
 
+
+
 class MapView extends React.Component {
+  
   render(){
-
     const providers = this.props.premProviders
-
+    const stdproviders = this.props.stdProviders
     const providerElements = _.map(providers, function(p,i){
       var latlng = [p.lat, p.lon]
-      console.log("Prem Providers: "+p.name)
-      return <Marker position={latlng} key={i}>
+      return <CircleMarker center={latlng} color='red' key={i}>
         <Popup>
-          <span>{(p.name)}<br />{(p.specialty)}<br />{(p.rating)}</span>
+          <span>{(p.name)}<br />{(p.specialty)}<br />{(p.rating)}<br />
+		  </span>
         </Popup>
-      </Marker>
+      </CircleMarker>
+    })
+	const stdElements = _.map(stdproviders, function(p,i){
+      var latlng = [p.lat, p.lon]
+      return <CircleMarker center={latlng} color='blue' key={i}>
+        <Popup>
+          <span>{(p.name)}<br />{(p.specialty)}<br />{(p.rating)}<br />
+		  </span>
+        </Popup>
+      </CircleMarker>
     })
 
     let userElement
     if (this.props.user){
-      userElement = <CircleMarker center={this.props.user.pos}/>
+      userElement = <Marker position={this.props.user.pos}/>
     } else {
       userElement = ''
     }
@@ -28,13 +39,14 @@ class MapView extends React.Component {
 
     if (this.props.user) {
     return  <div className="container grey darken-3">
-    <h2>Premium service providers:</h2><Map center={this.props.center}
+    <h2>Service providers:</h2><Map center={this.props.center}
           zoom={13}
           onLeafletClick={this.handleLeafletClick.bind(this)}>
         <TileLayer
           url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/>
         {providerElements}
+		{stdElements}
         {userElement}
       </Map></div>
     }
